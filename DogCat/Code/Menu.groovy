14682c479d53c9ed67b23ec7427ba0a3dd9f322c
torbuntu <sorensentor@tuta.io>
2019-12-29 16:41:45 -0600

    Removing older no longer needed demo projects.

--- DogCat/Code/Menu.groovy
+++
@@ -1,9 +0,0 @@
-class Menu {
-
-	boolean activeMenu = true
-	
-	def drawMenu(screen){
-		screen.drawString("Press ENTER to start!", 0, 80, 240, 1, 32)
-		screen.sprite(0, 100, 80, 2)
-	}
-}
--- Kotlin/Code/Kotlin.kt
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-open class Kotlin: leikr.Engine() {
-	override fun create(){}
-	override fun update(delta: Float){}
-	override fun render(){
-		val x:BigDecimal =  BigDecimal(0)
-		super.drawString("Hello, world!", x, x, 28)
-	}
-}
